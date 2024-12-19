e005c11f2dc11fc5f9a445845eff2f2f19e403cc
MAKRI <31958699+alimakri@users.noreply.github.com>
2024-12-19 09:26:59 +0100

    Add files via upload

---
+++ 14.ps1
@@ -0,0 +1,24 @@
+﻿# Exercice Mercredi : tri à bulle
+
+$tableau = @(64, 34, 25, 12, 22, 11, 90)
+$tableau
+$curseur = 0
+
+while($curseur -lt $tableau.Length-1)
+{
+    if ($tableau[$curseur] -gt $tableau[$curseur+1])
+    {
+        # Inversion des 2 éléments
+        $tampon = $tableau[$curseur]
+        $tableau[$curseur] = $tableau[$curseur+1]
+        $tableau[$curseur+1] = $tampon
+
+        $curseur=0
+    }
+    else
+    {
+        $curseur++
+    }
+}
+Write-Host "---------------------" -ForegroundColor Yellow
+$tableau
