9ddb79de0a4d46d60a4d02bd56a0ed9ff07c046e
MAKRI <31958699+alimakri@users.noreply.github.com>
2024-12-19 10:38:19 +0100

    Add files via upload

---
+++ 15.ps1
@@ -0,0 +1,26 @@
+﻿# Charger les assemblées nécessaires
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Créer la fenêtre
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Exemple WinForms"
+$form.Size = New-Object System.Drawing.Size(300, 200)
+$form.StartPosition = "CenterScreen"
+
+# Créer un bouton
+$button = New-Object System.Windows.Forms.Button
+$button.Size = New-Object System.Drawing.Size(100, 50)
+$button.Location = New-Object System.Drawing.Point(100, 70)
+$button.Text = "Cliquez-moi"
+
+# Ajouter un événement au bouton
+$button.Add_Click({
+    [System.Windows.Forms.MessageBox]::Show("Bonjour, monde!", "Message")
+})
+
+# Ajouter le bouton à la fenêtre
+$form.Controls.Add($button)
+
+# Afficher la fenêtre
+[void]$form.ShowDialog()
---
+++ 16.ps1
@@ -0,0 +1,20 @@
+﻿
+cls
+function pingAll
+{
+    param($liste)
+
+    $reponse=@()
+    foreach($pc in $listePC)
+    {
+        $pingSuccess = Test-Connection -ComputerName $pc.nom -Count 1 -Quiet
+        $reponse += @{
+                      Nom=$pc.nom 
+                      Etat= $pingSuccess
+                     }
+    }
+    return $reponse
+}
+$listePC = Import-Csv -Path "C:\Users\Administrateur\Desktop\Support\listePC.csv" 
+
+
